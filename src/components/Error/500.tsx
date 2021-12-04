ac39b2064ec47b958bcbe80e26193db3543ae365
Tran Phong <n.tranphongse@gmail.com>
2021-12-04 22:28:29 +0700

    Add react-helmet-async and add it to Error pages:

--- src/components/Error/500.tsx
+++ src/components/Error/500.tsx
@@ -2,39 +2,59 @@ import { MessageOutlined, RedoOutlined } from "@ant-design/icons";
 import { Button, Result } from "antd";
 
 import React from "react";
-
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import config from "../../config";
 const Error500: React.FC = () => (
-  <Result
-    status="500"
-    title="Lỗi máy chủ"
-    subTitle="Xin lỗi bạn, đã có lỗi xảy ra. Vui lòng thử lại sau."
-    extra={[
-      <Button
-        key="reload"
-        type="dashed"
-        onClick={() => window.location.reload()}
-        size="large"
-        icon={<RedoOutlined />}
-        style={{
-          marginRight: "10px",
-        }}
-      >
-        Tải lại trang
-      </Button>,
-      <Button
-        key="message"
-        type="primary"
-        href="m.me/HumansVTS"
-        size="large"
-        icon={<MessageOutlined />}
-        style={{
-          marginRight: "10px",
-        }}
-      >
-        Báo lỗi
-      </Button>,
-    ]}
-  />
+  <HelmetProvider>
+    <Helmet>
+      <title>Có lỗi xảy ra - {config.APP_NAME}</title>
+      <link rel="canonical" href="https://university.humansofvothisau.com" />
+      <meta
+        property="og:title"
+        content={`Có lỗi xảy ra - ${config.APP_NAME}`}
+      />
+      <meta
+        property="og:description"
+        content="Humans Of Vo Thi Sau đồng hành cùng các VTS-ers trong kỳ thi Tốt nghiệp THPT"
+      />
+      <meta
+        property="og:url"
+        content="https://university.humansofvothisau.com"
+      />
+      <meta name="robots" content="follow, noindex" />
+    </Helmet>
+    <Result
+      status="500"
+      title="Lỗi máy chủ"
+      subTitle="Xin lỗi bạn, đã có lỗi xảy ra. Vui lòng thử lại sau."
+      extra={[
+        <Button
+          key="reload"
+          type="dashed"
+          onClick={() => window.location.reload()}
+          size="large"
+          icon={<RedoOutlined />}
+          style={{
+            marginRight: "10px",
+          }}
+        >
+          Tải lại trang
+        </Button>,
+        <Button
+          key="message"
+          type="primary"
+          href="m.me/HumansVTS"
+          size="large"
+          icon={<MessageOutlined />}
+          style={{
+            marginRight: "10px",
+          }}
+        >
+          Báo lỗi
+        </Button>,
+      ]}
+    />
+  </HelmetProvider>
 );
 
 export default Error500;
--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,7 +1,8 @@
 import { MessageOutlined, RedoOutlined } from "@ant-design/icons";
 import { Button, Result } from "antd";
 import React from "react";
-
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import config from "../../config";
 type Props = {
   message?: string;
 };
@@ -19,37 +20,56 @@ const Error: React.FC<Props> = ({ message }) => {
     </div>
   );
   return (
-    <Result
-      status="error"
-      title="Đã có lỗi xảy ra"
-      subTitle={subTitleDisplay}
-      extra={[
-        <Button
-          key="reload"
-          type="dashed"
-          onClick={() => window.location.reload()}
-          size="large"
-          icon={<RedoOutlined />}
-          style={{
-            marginRight: "10px",
-          }}
-        >
-          Tải lại trang
-        </Button>,
-        <Button
-          key="message"
-          type="primary"
-          href="m.me/HumansVTS"
-          size="large"
-          icon={<MessageOutlined />}
-          style={{
-            marginRight: "10px",
-          }}
-        >
-          Báo lỗi
-        </Button>,
-      ]}
-    />
+    <HelmetProvider>
+      <Helmet>
+        <title>Có lỗi xảy ra - {config.APP_NAME}</title>
+        <link rel="canonical" href="https://university.humansofvothisau.com" />
+        <meta
+          property="og:title"
+          content={`Có lỗi xảy ra - ${config.APP_NAME}`}
+        />
+        <meta
+          property="og:description"
+          content="Humans Of Vo Thi Sau đồng hành cùng các VTS-ers trong kỳ thi Tốt nghiệp THPT"
+        />
+        <meta
+          property="og:url"
+          content="https://university.humansofvothisau.com"
+        />
+        <meta name="robots" content="follow, noindex" />
+      </Helmet>
+      <Result
+        status="error"
+        title="Đã có lỗi xảy ra"
+        subTitle={subTitleDisplay}
+        extra={[
+          <Button
+            key="reload"
+            type="dashed"
+            onClick={() => window.location.reload()}
+            size="large"
+            icon={<RedoOutlined />}
+            style={{
+              marginRight: "10px",
+            }}
+          >
+            Tải lại trang
+          </Button>,
+          <Button
+            key="message"
+            type="primary"
+            href="m.me/HumansVTS"
+            size="large"
+            icon={<MessageOutlined />}
+            style={{
+              marginRight: "10px",
+            }}
+          >
+            Báo lỗi
+          </Button>,
+        ]}
+      />
+    </HelmetProvider>
   );
 };
 
